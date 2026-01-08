f1641e1fbefd741f8c6dcd7305eab320f2f1c616
Ethan <mkaoxinice@gmail.com>
2026-01-09 00:13:59 +0800

    Add Gruvbox Theme and set it default

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to Load Gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
