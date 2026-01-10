aec085db98cfecbc2b05c10cc31620e03f2cb52b
Ethan <mkaoxinice@gmail.com>
2026-01-11 00:57:31 +0800

    bug fix

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+return {
+  -- "mason-org/mason.nvim",
+  -- opts = function(_, opts)
+  --   opts.ensure_installed = vim.list_extend(opts.ensure_installed or {}, {
+  --     "stylua", -- Add the packages you need
+  --   })
+  --   opts.ui = {
+  --     icons = {
+  --       package_installed = "✓",
+  --       package_pending = "➜",
+  --       package_uninstalled = "✗",
+  --     },
+  --   }
+  --   opts.log_level = vim.log.levels.ERROR -- Retains log level settings
+  -- end,
+}
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    spec = {
+      -- 给 <leader>K 添加一个 "书本" 或 "搜索" 图标
+      { "<leader>K", icon = "📖" },
+    },
+  },
+}
